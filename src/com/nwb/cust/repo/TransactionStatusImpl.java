9a2fd9764f93930d87e5c053fe17a0f5e3e0dc38
Anshu <anshugupta29a@gmail.com>
2024-08-27 09:25:41 +0530

    Added DAO layer to the Transaction Status

---
+++ src/com/nwb/cust/repo/TransactionStatusImpl.java
@@ -0,0 +1,13 @@
+package com.nwb.cust.repo;
+import com.nwb.cust.model.*;
+public class TransactionStatusImpl  implements TransactionStatusDAO {
+    @Override
+    public void addTransaction(Transaction transaction) throws SQLException {
+        // Code to insert transaction into database
+    }
+
+    @Override
+    public Transaction getTransactionById(String transactionId) throws SQLException {
+        // Code to retrieve transaction from database
+    }
+}
